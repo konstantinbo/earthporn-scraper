8c4e77c139eca25b45f406d415133af4f9271ae4
konsi1996 <s4234762@mail.zih.tu-dresden.de>
2016-06-09 23:32:40 +0200

    Print submission URLs and download images

--- scraper.py
+++ scraper.py
@@ -1,5 +1,14 @@
 import praw
-r = praw.Reddit(user_agent='my_cool_application')
-submissions = r.get_subreddit('opensource').get_hot(limit=5)
-[str(x) for x in submissions]
+import urllib
+r = praw.Reddit(user_agent='elementary-os:earthporn-scraper:v0.1 (by /u/k0nsi)')
+submissions = r.get_subreddit('earthporn').get_hot(limit=5)
+
+#print([str(x) for x in submissions])
+for x in submissions:
+    print(x.url)
+
+#downloads the image from the given url to the current directory
+urllib.request.urlretrieve("http://farm8.staticflickr.com//7465//27439812671_2917b68c09_k.jpg", "./pic.jpg");
+
+
 
