d5ae6d542be07c04cb81e1a5a7c8e755557313dc
konsi1996 <s4234762@mail.zih.tu-dresden.de>
2016-06-10 20:05:20 +0200

    Proper naming (closes #5)

--- scraper.py
+++ scraper.py
@@ -13,7 +13,7 @@ def get_subs(name, limit, r):
     return submissions
 
 
-def download_subs(subs):
+def download_subs(subs, location):
     loop = 0
     regexp = re.compile(r'((https://i\.)|(\.jpg)$)')
     for x in subs:
@@ -21,14 +21,24 @@ def download_subs(subs):
         print(loop)
         print(x.url)
         if regexp.search(x.url) is not None:
-            urllib.request.urlretrieve(x.url, './pic' + str(loop) + '.jpg')
+            filename = x.url.split('/')[-1]
+            filename = valid_image_name(filename)
+            urllib.request.urlretrieve(x.url, location + filename)
+
+
+def valid_image_name(filename):
+    regexp = re.compile(r'(jpg)$')
+    if regexp.search(filename) is None:
+        return filename + '.jpg'
+    else:
+        return filename
 
 
 def main():
     print("Starting call...")
     r = setup('elementary-os:earthporn-scraper:v0.2 (by /u/k0nsi)')
     subs = get_subs('earthporn', 5, r)
-    download_subs(subs)
+    download_subs(subs, './')
 
 if __name__ == '__main__':
     main()
