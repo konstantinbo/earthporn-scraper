e976236176fe5b121ea4c11d0010989d157cf833
konsi1996 <s4234762@mail.zih.tu-dresden.de>
2016-06-10 19:06:18 +0200

    First functionality with regex (#1)

--- scraper.py
+++ scraper.py
@@ -1,14 +1,36 @@
-import praw
-import urllib
-r = praw.Reddit(user_agent='elementary-os:earthporn-scraper:v0.1 (by /u/k0nsi)')
-submissions = r.get_subreddit('earthporn').get_hot(limit=5)
+#!/usr/bin/env python3
 
-#print([str(x) for x in submissions])
-for x in submissions:
-    print(x.url)
+import praw, re, urllib
 
-#downloads the image from the given url to the current directory
-urllib.request.urlretrieve("http://farm8.staticflickr.com//7465//27439812671_2917b68c09_k.jpg", "./pic.jpg");
 
+def setup(user_agent):
+    r = praw.Reddit(user_agent=user_agent)
+    return r
+
+
+def get_subs(name, limit, r):
+    submissions = r.get_subreddit(name).get_hot(limit=limit)
+    return submissions
+
+
+def download_subs(subs):
+    loop = 0
+    regexp = re.compile(r'((https://i\.)|(\.jpg)$)')
+    for x in subs:
+        loop += 1
+        print(loop)
+        print(x.url)
+        if regexp.search(x.url) is not None:
+            urllib.request.urlretrieve(x.url, './pic' + str(loop) + '.jpg')
+
+
+def main():
+    print("Starting call...")
+    r = setup('elementary-os:earthporn-scraper:v0.2 (by /u/k0nsi)')
+    subs = get_subs('earthporn', 5, r)
+    download_subs(subs)
+
+if __name__ == '__main__':
+    main()
 
 
